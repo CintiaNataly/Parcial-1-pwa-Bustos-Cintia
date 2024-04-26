a94c9c011a09bfbc823e74b267771dffe5f5bd59
CintiaNataly <164966730+CintiaNataly@users.noreply.github.com>
2024-04-25 23:30:17 -0300

    feat: Bueno, ahora parece que si se digno a funcionar el pedido de datos

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-https://api.sampleapis.com/simpsons/characters
+const URL_EPISODES = "https://api.sampleapis.com/simpsons/episodes";
+
+
+fetch(URL_EPISODES)
+    .then(datos => datos.json())
+    .then(resultado => {
+        const results = resultado.results;
+        console.log("results", resultado)
+
+    })
+
+
