8fe92ee50e3f520bdd1ad43896b8e04e3198010b
CintiaNataly <164966730+CintiaNataly@users.noreply.github.com>
2024-04-26 12:09:10 -0300

    feat: Estructura en html armada + css + img => lo tengo que pasar a DOM

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const URL_EPISODES = "https://api.sampleapis.com/simpsons/episodes";
 
 
 fetch(URL_EPISODES)
-    .then(datos => datos.json())
-    .then(resultado => {
-        const results = resultado.results;
-        console.log("results", resultado)
-
+    .then(data => data.json())
+    .then(result => {
+        const results = result.slice(0,99);
+        console.log(results);
     })
+
 
 
